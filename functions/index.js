c009c38a67621bbb4afade2c0bd875b7a1d33b0d
WeiXie03 <wei.tom.xie@gmail.com>
2018-03-02 01:34:12 +0000

    Stable version, single street traffic, fixed traffic light timing, added Google map of intersection on end-user.

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,11 @@ var numstates = 3;
 var state = 0; //0 = green, 1 = yellow, 2 = red
 var twait = [20000,3000,20000];
 
+var eastPoint;
+var intersecLoc;
+var usrLoc;
+var thetaEastroad; // The theta offset between real east and the direction of the eastward road.
+
 function update() {
     var curr = Date.now();
     if(tlast == -1) {
@@ -27,7 +32,37 @@ function update() {
         tnext = twait[state] + tlast;
         console.log('State is ', state);
     }
-    io.emit('check', {state:state, next:tnext});
+    io.emit('stateUpdate', {state:state, next:tnext});
+}
+
+function localCoords(point){
+    return {lon: point.lon-intersecLoc.lon, lat: point.lat-intersecLoc.lat}
+}
+
+function getRoad(eastPoint, intersecLoc, usrLoc) {
+    usrLocl = localCoords(usrLoc);
+    // lat is y and lon is x
+    thetaUsr = Math.degrees(Math.atan2(usrLocl.lat, usrLocl.lon));
+    thetaUsr = ((thetaUsr % 360) + 360) % 360;
+
+    relUsrTheta = thetaUsr - thetaEastroad;
+    relUsrTheta = ((relUsrTheta % 360) + 360) % 360;
+    
+    var usrRoad;
+    console.log('usrTheta:', relUsrTheta)
+    if (relUsrTheta >= (45) && relUsrTheta < (45 + 90)) {
+        usrRoad = 'north';
+    } else if (relUsrTheta >= (135) && relUsrTheta < (135 + 90)) {
+        usrRoad = 'west';
+    } else if (relUsrTheta >= (135 + 90) && relUsrTheta < (135 + 180)) {
+        usrRoad = 'south';
+    } else if (relUsrTheta >= (270 + 45) && relUsrTheta < (360)) {
+        usrRoad = 'east'; 
+    } else if (relUsrTheta >= (0) && (relUsrTheta < (45))) {
+        usrRoad = 'east';
+    }
+    console.log('usrRoad:', usrRoad)
+    return usrRoad
 }
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
@@ -40,13 +75,49 @@ http.listen(8080, '127.0.0.1', function() {
 
 io.on('connection', function(socket) {
     console.log('iz hooked up');
-    socket.on('setServerLocation', function(position) {
-        console.log(position);
-        console.log(position.latitude, position.longitude);
+
+    socket.on('setIntersecLoc', function(position) {
+        console.log(position.lat, position.lon);
+        intersecLoc = position;
+        io.emit('updateLoc', position);
     });
+
+    socket.on('setEast', function(position) {
+        console.log(position.lat, position.lon);
+        eastPoint = position;
+        eastPointl = localCoords(eastPoint)
+        // TODO send update east error message if eastPointl is very close to zero length
+        thetaEastroad = Math.degrees(Math.atan2(eastPointl.lat, eastPointl.lon));
+        thetaEastroad = (thetaEastroad + 360) % 360;
+        socket.emit('updateEast', `east set angle is: ${thetaEastroad}`);
+    });
+
+/*    socket.on('updateUsrLoc', function(position) {
+        console.log(position.latitude, position.longitude);
+        usrLoc = position;
+    });
+*/
+
+    socket.on('getRoad', function(position) {
+       usrLoc = position;
+       socket.emit('roadUpdate', getRoad(eastPoint, intersecLoc, usrLoc)); 
+    });
+
+    socket.on('getIntersecLoc', function(position) {
+       socket.emit('updateIntersecLoc', intersecLoc); 
+    });
+
     socket.on('disconnect', function() {
         console.log('dis user\'s gon\' now');
     });
 });
 
 setInterval(update, 100);
+
+Math.radians = function(degrees) {
+    return degrees * Math.PI / 180;
+};
+
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
